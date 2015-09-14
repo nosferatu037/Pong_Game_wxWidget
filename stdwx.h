22248ee1657742b3b47c6dc0cd1cc58026472c30
nosferatu037 <nosferatu037@gmail.com>
2015-09-13 22:46:27 -0700

    List of headers for the precompilation

---
+++ stdwx.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   stdwx.h
+ * Author: nosferatu
+ *
+ * Created on July 10, 2015, 7:48 PM
+ */
+
+#ifndef STDWX_H
+#define	STDWX_H
+
+#include <wx/wx.h>
+#include <wx/log.h>
+#include <wx/app.h>
+#include <wx/event.h>
+#include <wx/image.h>
+#include <wx/filefn.h>
+#include <wx/defs.h>
+#include <wx/string.h>
+#include <wx/buffer.h>
+#include <wx/timer.h>
+
+#endif	/* STDWX_H */
+
